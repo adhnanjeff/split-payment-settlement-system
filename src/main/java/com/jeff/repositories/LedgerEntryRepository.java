a7a171a62a9a3a35d6342eb9be4628b45e80c25f
Adhnan Jeff M S <adhnanjeff26@gmail.com>
2026-01-05 21:35:48 +0530

    feat: added repos

---
+++ src/main/java/com/jeff/repositories/LedgerEntryRepository.java
@@ -0,0 +1,11 @@
+package com.jeff.repositories;
+
+import com.jeff.entities.LedgerEntry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LedgerEntryRepository extends JpaRepository<LedgerEntry, UUID> {
+}
